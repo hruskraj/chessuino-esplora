0a651c13b71ca8f1b4bdf0a7dff12833ff1daced
Rajmund Hubert Hruška <hruskraj@fit.cvut.cz>
2018-04-08 22:58:50 +0200

    Add colors

--- main/definitions.h
+++ main/definitions.h
@@ -1,1 +1,7 @@
+struct PileColor{
+  byte r, g, b;
+  PileColor(byte r, byte g, byte b) : r(r), g(g), b(b){};
+};
 
+const PileColor blackColor(224, 155, 6);
+const PileColor whiteColor(255, 220, 145);
