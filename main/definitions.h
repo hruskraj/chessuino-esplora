a153ab9a6fed87a4049db4e498e1c9ec7d4aa542
Rajmund Hubert Hruška <hruskraj@fit.cvut.cz>
2018-04-08 23:01:28 +0200

    Fix typos

--- main/definitions.h
+++ main/definitions.h
@@ -1,7 +1,7 @@
-struct PileColor{
+struct TileColor{
   byte r, g, b;
-  PileColor(byte r, byte g, byte b) : r(r), g(g), b(b){};
+  TileColor(byte r, byte g, byte b) : r(r), g(g), b(b){};
 };
 
-const PileColor blackColor(224, 155, 6);
-const PileColor whiteColor(255, 220, 145);
+const TileColor lightColor(224, 155, 6);
+const TileColor darkColor(255, 220, 145);
