1075a0e4b778ed7fead5f06b50ae93a125bdfba5
Rajmund Hubert Hruška <hruskraj@fit.cvut.cz>
2018-04-10 22:29:05 +0200

    Fix RGB values of light and dark tiles

--- main/definitions.h
+++ main/definitions.h
@@ -3,5 +3,5 @@ struct TileColor{
   TileColor(byte r, byte g, byte b) : r(r), g(g), b(b){};
 };
 
-const TileColor lightColor(224, 155, 6);
-const TileColor darkColor(255, 220, 145);
+const TileColor lightColor(255, 220, 145);
+const TileColor darkColor(224, 155, 6);
