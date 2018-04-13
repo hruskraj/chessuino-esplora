c64c0c83f69876693add6e1d55cae8004aff8a6f
Rajmund Hubert Hruška <hruskraj@fit.cvut.cz>
2018-04-13 21:20:34 +0200

    Add state transitions

--- main/main.ino
+++ main/main.ino
@@ -8,23 +8,131 @@
 #include <SPI.h>
 #include "definitions.h"
 
-enum states STATE, NEXT_STATE;
+///Current state.
+enum states STATE;
+///Next state.
+enum states NEXT_STATE;
 
+/**
+ * @brief Setup.
+ * 
+ * Sets MENU_NEW_GAME as current state.
+ * @sa states
+ */
 void setup(){
   EsploraTFT.begin();
   EsploraTFT.background(0, 0, 0);
-  drawBoard();
-  drawEveryPiece();
+  /*drawBoard();
+  drawEveryPiece();*/
+  STATE = NEXT_STATE = MENU_NEW_GAME;
+  menu1Init();
 }
-
+/**
+ * @brief Main loop.
+ * 
+ * @sa states
+ */
 void loop(){
   switch(STATE){
-    case MENU_1:
+    case MENU_NEW_GAME:
+      if(buttonPressed(SWITCH_2)){
+        NEXT_STATE = MENU_VS_PLAYER;
+        menu2Init();
+      }
+      if(buttonPressed(SWITCH_1)){
+        NEXT_STATE = MENU_CONTROLS;
+        menu1MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_3)){
+        NEXT_STATE = MENU_ABOUT;
+        menu1MoveSelector(NEXT_STATE);
+      }
       break;
-    case MENU_2:
+      
+    case MENU_CONTROLS:
+      if(buttonPressed(SWITCH_2)){
+        NEXT_STATE = CONTROLS;
+        //controlsInit();
+      }
+      if(buttonPressed(SWITCH_1)){
+        NEXT_STATE = MENU_ABOUT;
+        menu1MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_3)){
+        NEXT_STATE = MENU_NEW_GAME;
+        menu1MoveSelector(NEXT_STATE);
+      }
       break;
-    case GAME:
+      
+    case MENU_ABOUT:
+      if(buttonPressed(SWITCH_2)){
+        NEXT_STATE = ABOUT;
+        //aboutInit();
+      }
+      if(buttonPressed(SWITCH_1)){
+        NEXT_STATE = MENU_NEW_GAME;
+        menu1MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_3)){
+        NEXT_STATE = MENU_CONTROLS;
+        menu1MoveSelector(NEXT_STATE);
+      }
       break;
+
+    case MENU_VS_PLAYER:
+      if(buttonPressed(SWITCH_1)){
+        NEXT_STATE = MENU_VS_AI_EASY;
+        menu2MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_2)){
+        
+      }
+      if(buttonPressed(SWITCH_3)){
+        NEXT_STATE = MENU_VS_AI_HARD;
+        menu2MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_4)){
+        NEXT_STATE = MENU_NEW_GAME;
+        menu1Init();
+      }
+      break;
+      
+    case MENU_VS_AI_EASY:
+      if(buttonPressed(SWITCH_1)){
+        NEXT_STATE = MENU_VS_AI_HARD;
+        menu2MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_2)){
+
+      }
+      if(buttonPressed(SWITCH_3)){
+        NEXT_STATE = MENU_VS_PLAYER;
+        menu2MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_4)){
+        NEXT_STATE = MENU_NEW_GAME;
+        menu1Init();
+      }
+      break;
+
+    case MENU_VS_AI_HARD:
+      if(buttonPressed(SWITCH_1)){
+        NEXT_STATE = MENU_VS_AI_EASY;
+        menu2MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_2)){
+        
+      }
+      if(buttonPressed(SWITCH_3)){
+        NEXT_STATE = MENU_VS_PLAYER;
+        menu2MoveSelector(NEXT_STATE);
+      }
+      if(buttonPressed(SWITCH_4)){
+        NEXT_STATE = MENU_NEW_GAME;
+        menu1Init();
+      }
+      break;
+    
   }
   STATE = NEXT_STATE;
 }
