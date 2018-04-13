cdecea5995dd98782a2b4a6eff69e78c1f7a1188
Rajmund Hubert Hruška <hruskraj@fit.cvut.cz>
2018-04-13 16:07:37 +0200

    Optimize code

--- main/chessPieces.ino
+++ main/chessPieces.ino
@@ -14,34 +14,34 @@
  * @param k position on x axis
  * @return int representing value for RGB
  */
-int getTableEntry(int i, int j, int k){
+byte getTableEntry(byte i, byte j, byte k){
   return pgm_read_word(&chessPieces[i][j][k]);
 }
 /**
  * @brief Get color of specific tile.
  * 
- * @param i row
- * @param j column
+ * @param r row
+ * @param c column
  * @return TileColor color
  */
-const TileColor & getTileColor(short i, short j){
-  return (i + j) % 2 == 0 ? lightColor : darkColor;  
+const TileColor & getTileColor(int r, int c){
+  return (r + c) % 2 == 0 ? lightColor : darkColor;  
 }
 /**
  * @brief Draws chess piece at given location.
  * 
- * @param l row
- * @param k column
- * @param piece piece
+ * @param r row
+ * @param c column
+ * @param p piece
  */
-void drawPiece(int l, int k, int piece){
+void drawPiece(byte r, byte c, byte p){
   for(int i = 0; i < 16; ++i){
     for(int j = 0; j < 16; ++j){
-      int t = getTableEntry(piece, i, j);
+      byte t = getTableEntry(p, i, j);
       if(t == noColor)
         continue;   
       EsploraTFT.stroke(t, t, t);
-      EsploraTFT.point(k * 16 + i, l * 16 + j);
+      EsploraTFT.point(r * 16 + i, c * 16 + j);
     }
   }
 }
@@ -49,14 +49,14 @@ void drawPiece(int l, int k, int piece){
  * @brief Draws every chess piece on default location.
  */
 void drawEveryPiece(){
-  for(int k = 0; k < 2; ++k){
-    for(int i = 0; i < 3; ++i){
+  for(byte k = 0; k < 2; ++k){
+    for(byte i = 0; i < 3; ++i){
       drawPiece(0 + k * 7, i, i + k * 6);
       drawPiece(0 + k * 7, 7 - i, i + k * 6);
     }
     drawPiece(0 + k * 7, 3, 3 + k * 6);
     drawPiece(0 + k * 7, 4, 4 + k * 6);
-    for(int i = 0; i < 8; ++i)
+    for(byte i = 0; i < 8; ++i)
       drawPiece(1 + k * 5, i, 5 + k * 6);
   }
 }
@@ -69,17 +69,17 @@ void drawEveryPiece(){
  * [0,0] and bottom right square coordinates are [7,7].
  */
 void drawBoard(){
-  bool white = true;
+  bool light = true;
   EsploraTFT.noStroke();
   for(int i = 0; i < 8; ++i){
     for(int j = 0; j < 8; ++j){
-      if(white)
+      if(light)
         EsploraTFT.fill(lightColor.r, lightColor.g, lightColor.b);
       else
         EsploraTFT.fill(darkColor.r, darkColor.g, darkColor.b);
       EsploraTFT.rect(i * 16, j * 16, 16, 16);
-      white = !white;
+      light = !light;
     }
-    white = !white;
+    light = !light;
   }
 }
